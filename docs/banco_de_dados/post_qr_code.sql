39d299cca388268d8735785c26dac3c2010d2f1b
Diego Brocanelli <diegod2@msn.com>
2018-01-14 09:06:04 -0200

    script para geração da tabela de exemplo a ser utilizada no post.

---
+++ docs/banco_de_dados/post_qr_code.sql
@@ -0,0 +1,23 @@
+--
+-- Table structure for table `cadastro`
+--
+
+DROP TABLE IF EXISTS `cadastro`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cadastro` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `matricula` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cadastro`
+--
+
+LOCK TABLES `cadastro` WRITE;
+/*!40000 ALTER TABLE `cadastro` DISABLE KEYS */;
+INSERT INTO `cadastro` VALUES (1,'649969'),(2,'345768'),(3,'677623'),(4,'266501'),(5,'672716'),(6,'603669'),(7,'268426'),(8,'593472'),(9,'239878'),(10,'746675');
+/*!40000 ALTER TABLE `cadastro` ENABLE KEYS */;
+UNLOCK TABLES;
